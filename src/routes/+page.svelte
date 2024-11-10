de3dd4178fd7f4a16b06e5cebcbc563749b557c7
Levi Villarreal <villarreallevi@gmail.com>
2024-11-10 04:33:17 -0600

    I guess routes are needed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,4 @@
 <script lang="ts">
-  // Disable server-side rendering for static site
-  export const ssr = false;
-  export const prerender = true;
-
   import App from "../components/App.svelte";
 </script>
 
--- svelte.config.js
+++ svelte.config.js
@@ -13,10 +13,13 @@ const config = {
       // these options are set automatically — see below
       pages: "build",
       assets: "build",
-      fallback: undefined,
+      fallback: "index.html",
       precompress: false,
       strict: true,
     }),
+    prerender: {
+      default: true,
+    },
     paths: {
       base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
     },
