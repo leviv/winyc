6be384e18c0af3ed2495064cfab8651237bc0dd8
Levi Villarreal <villarreallevi@gmail.com>
2024-11-10 04:25:41 -0600

    Add git workflow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   // Disable server-side rendering for static site
   export const ssr = false;
+  export const prerender = true;
 
-  import App from "./App.svelte";
+  import App from "../components/App.svelte";
 </script>
 
 <App />
